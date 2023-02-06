229f85bc441aedcb65b0b3008ac2413b4941d484
Yasaswani Rongali <92283060+yswnr@users.noreply.github.com>
2023-02-06 22:51:50 +0530

    Update flip.py

--- Python_DS_Assignment_Question_02/my_package/data/transforms/flip.py
+++ Python_DS_Assignment_Question_02/my_package/data/transforms/flip.py
@@ -11,12 +11,22 @@ class FlipImage(object):
             Arguments:
             flip_type: 'horizontal' or 'vertical' Default: 'horizontal'
         '''
+        self.flip_type = flip_type
 
     def __call__(self, image):
         '''
             Arguments:
             image (numpy array or PIL image)
-
             Returns:
             image (numpy array or PIL image)
         '''
+        if self.flip_type=='horizontal':
+           flipped_image = image.transpose(Image.FLIP_LEFT_RIGHT)
+        elif self.flip_type=='vertical':
+           flipped_image = image.transpose(Image.FlIP_TOP_BOTTOM)
+        elif self.flip_type=='anticlockwise':
+           flipped_image = image.transpose(Image.TRANSPOSE)
+        elif self.flip_type=='clockwise':
+           flipped_image=image.transpose(Image.TRANSVERSE)
+        return flipped_image            
+
