8a9b16298e0e66560d924d06570216b070d20d9f
Yasaswani Rongali <92283060+yswnr@users.noreply.github.com>
2023-02-06 23:05:44 +0530

    Update setup.py

--- Python_DS_Assignment_Question_02/my_package/data/setup.py
+++ Python_DS_Assignment_Question_02/my_package/data/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
      
-  name ='my_package',
+  name ='my_package.data.transforms',
   version='0.1'
   package=find_packages()
   include_package_data = True,
